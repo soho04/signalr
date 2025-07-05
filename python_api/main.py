9840ce4ffb2f2dc68fa99bb3d5882d31b050dc60
psyas26 <psyas26@nottingham.ac.uk>
2025-07-05 15:01:39 +0100

    Working on News feature

--- python_api/main.py
+++ python_api/main.py
@@ -1,8 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import requests
+import httpx
+from datetime import date, timedelta
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI!"}
+FINNHUB_API_KEY = "d1kht4hr01qt8foomrh0d1kht4hr01qt8foomrhg"
+
+# Get latest news from external APIs
+@app.get("/news/{ticker}")
+async def get_news(ticker: str):
+    
+    today, aweekago = date.today(), date.today() - timedelta(days=7)
+    today_str, aweekago_str = today.isoformat(), aweekago.isoformat()
+    
+    
+    url = "https://finnhub.io/api/v1/company-news"
+    params = {
+        "symbol": ticker.upper(),
+        "from": aweekago_str,
+        "to": today_str,
+        "token": FINNHUB_API_KEY,
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, params=params)
+        
+        if response.status_code != 200:
+            raise HTTPException(status_code=response.status_code, detail="Error fetching news")
+        
+        news_data = response.json()
+
+        return {"ticker": "PLTR", "news": news_data[:5]}
---
+++ python_api/news.py
@@ -0,0 +1,3 @@
+# Retrieve news articles from different sources and merge
+
+FINNHUB_API_KEY = "d1kht4hr01qt8foomrh0d1kht4hr01qt8foomrhg"
