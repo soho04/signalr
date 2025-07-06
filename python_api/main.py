0364da646218cd349ef51c3a2a34028019d18361
psyas26 <psyas26@nottingham.ac.uk>
2025-07-07 00:02:19 +0100

    Switching up scenery to C++

--- python_api/main.py
+++ python_api/main.py
@@ -4,16 +4,30 @@ import httpx
 from datetime import date, timedelta
 from news.getnews import get_finnhub_news as fetch_finnhub_news, get_marketaux_news as fetch_marketaux_news, combine_news_sources
 from news.newsengine import save_news_to_db, dynamic_news
+from contextlib import asynccontextmanager
+import asyncio
 
+async def run_during_runtime():
+   while True:
+      await dynamic_news()
+      await asyncio.sleep(300)  # Run every hour
 
-app = FastAPI()
-
-@app.get("/news/finnhub")
-async def get_finnhub_news():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
    await save_news_to_db()
+   dynamic_update = asyncio.create_task(run_during_runtime())
+   app.state.task = dynamic_update
+   try:
+      yield
 
-   return {"message": "News saved to database successfully."}
+   finally:
+      dynamic_update.cancel()
+      try:
+         await dynamic_update
+      except asyncio.CancelledError:
+         print("Task cancelled")
 
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/news/marketaux")
